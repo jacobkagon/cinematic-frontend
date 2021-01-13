f9c660fbb238b75e045ba78fae11cdc9f66293e7
JakesProjects1 <jacob@kagon.net>
2021-01-13 10:40:34 -0500

    changed the scroll format of movies

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -6,10 +6,12 @@ import IconButton from "@material-ui/core/IconButton";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import InfoIcon from "@material-ui/icons/Info";
-import {Rating} from "@material-ui/lab"
-import {Link} from "react-router-dom"
-import MovieDetails from "./MovieDetails"
-import {URL_IMG, IMG_SIZE_LARGE} from '../const'
+import { Rating } from "@material-ui/lab";
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { Link } from "react-router-dom";
+import MovieDetails from "./MovieDetails";
+
+import { URL_IMG, IMG_SIZE_LARGE } from "../const";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,22 +21,25 @@ const useStyles = makeStyles((theme) => ({
     overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
   },
+ 
   gridList: {
-    width: 800,
-    height: 900,
+    flexWrap: "nowrap",
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: "translateZ(0)",
+    width: 1000,
+    height: 1500,
   },
+  
+  
   icon: {
     color: "rgba(255, 255, 255, 0.54)",
-  },
-  StarIcon: {
-    fontSize: "small"
   },
 }));
 
 export default function MovieGrid({ movies }) {
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
-  const [movieShow, handleMovieShow] = React.useState(false)
+  const [movieShow, handleMovieShow] = React.useState(false);
 
   const handleChange = (event) => {
     setSpacing(Number(event.target.value));
@@ -43,37 +48,30 @@ export default function MovieGrid({ movies }) {
   const body = "hello";
 
   const handleShowPage = () => {
-      handleMovieShow(true)
-      if(movieShow === true) {
-          handleMovieShow(false)
-      }
-  }
+    handleMovieShow(true);
+    if (movieShow === true) {
+      handleMovieShow(false);
+    }
+  };
 
   return (
-    <div>
-   {movieShow === true ? <MovieDetails/> : null}
     <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">Trending</ListSubheader>
-        </GridListTile>
-        
-        {movies.map((movie, id) => (
-            
-          <GridListTile key={id} style={{height: '400px'}}>
-           
+
+      <GridList className={classes.gridList} cols={4.5}>
+      
+        {movies.map((movie) => (
+          <GridListTile key={movie.id} style={{height: '300px'}}>
+
             <img
               src={URL_IMG + IMG_SIZE_LARGE + movie.poster_path}
               alt={movie.title}
             />
             <GridListTileBar
               title={movie.title}
-              subtitle={
-                <span>
-                 
-                 <Rating name="half-rating-read" defaultValue={movie.vote_average / 2} precision={0.5} readOnly />
-                </span>
-              }
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
               actionIcon={
                 <Link to={'/movie/'+movie.id}>
                 <IconButton
@@ -87,9 +85,7 @@ export default function MovieGrid({ movies }) {
             />
           </GridListTile>
         ))}
-        
       </GridList>
     </div>
-</div>
   );
 }
